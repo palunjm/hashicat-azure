8171504ca942b0cb0cdfc9806284af830cb21f6e
palunjm <Jeff.Palun@kroger.com>
2020-08-26 13:27:54 -0400

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own. 
+  Welcome to end of the internet. Your search is over. Go back from here.
   <!-- END -->
   
   </div>
